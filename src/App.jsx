e64184448abee45d78ac23f917e7ff2e4c22d772
Jude Andersen <jude.ottilie@gmail.com>
2026-03-25 13:59:10 -0500

    before formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { useEffect, useState } from "react"
 import "./App.css"
-import { postJoke } from "./services/jokeService"
+import { postJoke, getJokes } from "./services/jokeService"
 
 export const App = () => {
   const [newJoke, setNewJoke] = useState("")
+  const [allJokesArray, setAllJokesArray] = useState([])
+  const [toBeToldJokesArray, setToBeToldJokesArray] = useState([])
+  const [toldJokesArray, setToldJokesArray] = useState([])
+
+useEffect(() => {
+  getJokes().then((jokesArray) => {
+    setAllJokesArray(jokesArray)
+  })
+}, [])
+
+useEffect(() => {
+  setToldJokesArray(allJokesArray.filter((joke) => joke.told === true))
+  setToBeToldJokesArray(allJokesArray.filter((joke) => joke.told === false))
+}, [allJokesArray])
 
   return (
     <div className="app-container">
@@ -13,6 +27,7 @@ export const App = () => {
 
       <div className="joke-add-form">
         <input
+          value={newJoke}
           className="joke-input"
           type="text"
           placeholder="New One Liner"
@@ -20,17 +35,50 @@ export const App = () => {
             setNewJoke(event.target.value)
           }}
         />
-        <button
+         <button
           className="joke-input-submit"
           onClick={() => {
             postJoke({
             text: newJoke,
             told: false
             })
+            setNewJoke("")
           }}
         >
           Add Joke
         </button>
+        </div>
+        <div className="joke-lists-container">
+            <div className="joke-list-container">
+              <h2>Untold <span className="untold-count">{toBeToldJokesArray.length}</span></h2>
+              <ul>
+                {toBeToldJokesArray.map((joke) => {
+                  return (
+                   <li className="joke-list-item" key={joke.id}>
+      <p className="joke-list-item-text">{joke.text}</p>
+    </li> 
+                  )
+                })}
+              </ul>
+            </div>
+            <div className="joke-list-container">
+              <h2>
+                Told <span className="told-count">{toldJokesArray.length}
+                </span>
+              </h2>
+              <ul>
+                {toldJokesArray.map((joke) => {
+                  return (
+                  <li className="joke-list-item" key={joke.id}>
+                    <p className="joke-list-item-text">{joke.text}
+                    </p>
+
+                  </li>
+                  )
+                }
+              )}
+              </ul>
+            </div>
       </div>
     </div>
   )
--- src/services/jokeService.jsx
+++ src/services/jokeService.jsx
@@ -1,9 +1,17 @@
+//this module is responsible for talking to the database
+
 export const postJoke = (joke) => {
-      fetch(`http://localhost:8088/jokes`, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(joke)
-})  
-    } 
+  fetch(`http://localhost:8088/jokes`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(joke),
+  })
+}
+
+export const getJokes = () => {
+  return fetch(`http://localhost:8088/jokes`).then((response) => {
+    return response.json()
+  })
+}
